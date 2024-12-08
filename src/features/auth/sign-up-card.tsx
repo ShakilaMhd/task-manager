b1b7f55252e05ce8adddcbf13ad997354affecdc
shakila mohammadi <57131482+ShakilaMhd@users.noreply.github.com>
2024-12-08 15:24:30 +0330

    complete the sign-in-up

--- src/features/auth/sign-up-card.tsx
+++ src/features/auth/sign-up-card.tsx
@@ -42,7 +42,7 @@ const SignUpCard = () => {
   const onSubmit = (values: z.infer<typeof formSchema>) => {
     console.log({ values });
   };
-  
+
   return (
     <Card className="w-full h-full md:w-[487px] border-none shadow-none">
       <CardHeader className="flex items-center justify-center text-center p-7">
@@ -64,7 +64,6 @@ const SignUpCard = () => {
       <CardContent className="p-7">
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-
             <FormField
               name="name"
               control={form.control}
@@ -144,6 +143,18 @@ const SignUpCard = () => {
           Login with Github
           <FaGithub className="mr-2 size-5" />
         </Button>
+      </CardContent>
+
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7 flex items-center justify-center">
+        <p>
+          Already have an account?
+          <Link href={"/sign-in"}>
+            <span className="text-blue-700">&nbsp;sign-in</span>
+          </Link>
+        </p>
       </CardContent>
     </Card>
   );

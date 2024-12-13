242f1369996bfc0c7792158cf7ced33dd892da8f
shakila mohammadi <57131482+ShakilaMhd@users.noreply.github.com>
2024-12-13 23:18:15 +0330

    complete the use-logout api to delete the session and clear the current user and rputer push to /sign-in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 "use client"
 
+import { Button } from "@/components/ui/button";
 import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
   const { data, isLoading } = useCurrent();
-
+const {mutate} = useLogout()
   useEffect(() => {
     if (!data && !isLoading) {
       router.push("/sign-in");
     }
   }, [data]);
-  return <>only visible to authorized users</>;
+  return <>only visible to <Button onClick={() => mutate()}>logout</Button>authorized users</>;
 }

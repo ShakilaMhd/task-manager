d933d0d28689fde67c1b6d5a7f2eb90419236e13
shakila mohammadi <57131482+ShakilaMhd@users.noreply.github.com>
2024-12-13 23:02:17 +0330

    complete the loggedout route and create the current api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+"use client"
+
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
-  return <></>;
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+  return <>only visible to authorized users</>;
 }

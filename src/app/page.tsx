0d22d918990c1295ce70e9636e618723a7b2f2c4
shakila mohammadi <57131482+ShakilaMhd@users.noreply.github.com>
2024-12-19 19:12:45 +0330

    update my project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,3 @@
-"use client"
-
-import { Button } from "@/components/ui/button";
-import { useCurrent } from "@/features/auth/api/use-current";
-import { useLogout } from "@/features/auth/api/use-logout";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
-export default function Home() {
-  const router = useRouter();
-  const { data, isLoading } = useCurrent();
-const {mutate} = useLogout()
-  useEffect(() => {
-    if (!data && !isLoading) {
-      router.push("/sign-in");
-    }
-  }, [data]);
-  return <>only visible to <Button onClick={() => mutate()}>logout</Button>authorized users</>;
+export default async function Home() {
+  return <><h1>world</h1></>;
 }

035796906c998b90ef40139798121ae6d626ba52
shakila mohammadi <57131482+ShakilaMhd@users.noreply.github.com>
2024-12-21 00:13:50 +0330

    create the user button for better scence and create the actions component for control the switching page after login and logout

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -2,12 +2,14 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
 
 import { client } from "@/lib/rpc"
+import { useRouter } from "next/navigation";
 
 type ResponseType = InferResponseType<typeof client.api.auth.logout["$post"]>
 // type RequestType = InferRequestType<typeof client.api.auth.logout["$post"]>
 
 export const useLogout = () => {
-    const queryclient = useQueryClient()
+    const router = useRouter()
+    const queryClient = useQueryClient()
 
     const mutation = useMutation<ResponseType, Error>({
         mutationFn: async () => {
@@ -15,7 +17,9 @@ export const useLogout = () => {
             return await response.json()
         },
         onSuccess: () => {
-            queryclient.invalidateQueries({ queryKey: ["current"] })
+            router.refresh()
+            // window.location.reload()
+            queryClient.invalidateQueries({ queryKey: ["current"] })
         }
     })
     return mutation

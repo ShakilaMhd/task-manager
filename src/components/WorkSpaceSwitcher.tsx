927ae69d5d096f3d41bbba4e3ef94256f02a38e8
shakila mohammadi <57131482+ShakilaMhd@users.noreply.github.com>
2024-12-26 15:01:00 +0330

    ADD Avatar image for workspaces

--- src/components/WorkSpaceSwitcher.tsx
+++ src/components/WorkSpaceSwitcher.tsx
@@ -11,6 +11,8 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { WorkspaceAvatar } from "@/features/workspaces/components/workspace-avatar";
+import { wrap } from "module";
 
 export const WorkSpaceSwitcher = () => {
   const { data: workspaces } = useGetWorkspaces();
@@ -22,13 +24,22 @@ export const WorkSpaceSwitcher = () => {
         <RiAddCircleFill className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition" />
       </div>
       <Select>
-        <SelectTrigger dir="rtl" className="w-full  bg-neutral-200 font-medium p-1">
-          <SelectValue  placeholder="فضای کاری " className="items-center" />
+        <SelectTrigger
+          dir="rtl"
+          className="w-full  bg-neutral-200 font-medium p-1"
+        >
+          <SelectValue placeholder="فضای کاری " className="items-center" />
         </SelectTrigger>
         <SelectContent>
           {workspaces?.documents.map((workspace) => (
             <SelectItem key={workspace.$id} value={workspace.$id}>
-              {workspace.name}
+              <div className="flex justify-start items-center gap-3 font-medium">
+                <WorkspaceAvatar
+                  name={workspace.name}
+                  image={workspace.imageUrl}
+                />
+                <span className="truncate">{workspace.name}</span>
+              </div>
             </SelectItem>
           ))}
         </SelectContent>

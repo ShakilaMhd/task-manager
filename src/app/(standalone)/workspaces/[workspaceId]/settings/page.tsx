bbf8a832d6b04792aa3ea021168aa510481d657d
shakila mohammadi <57131482+ShakilaMhd@users.noreply.github.com>
2024-12-29 00:28:30 +0330

    ADD settings for edite the workspace Forms

--- src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 
 import { getCurrent } from "@/features/auth/actions";
+import { getWorkspace } from "@/features/workspaces/actions";
+import { EditWorkSpaceForm } from "@/features/workspaces/components/edit-workspace-form";
 
 interface WorkspaceIdSettingsPageProps {
   params: {
@@ -15,7 +17,13 @@ const WorkspaceIdSettingsPage = async ({
 
   if (!user) redirect("/sign-in");
 
-    return <div>WorkspaceIdSettingsPage:{params.workspaceId}</div>;
+  const initialValues = await getWorkspace({ workspaceId: params.workspaceId });
+
+  if (!initialValues) {
+    redirect(`/workspaces/${params.workspaceId}`);
+  }
+
+  return <EditWorkSpaceForm initialValues={initialValues}/>
 };
 
 export default WorkspaceIdSettingsPage;

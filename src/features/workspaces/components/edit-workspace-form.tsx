8de3fb9b949cee3f61c4af694474d660d3b85c0c
shakila mohammadi <57131482+ShakilaMhd@users.noreply.github.com>
2024-12-30 23:30:04 +0330

    ADD use-delete-workspace compo and api

--- src/features/workspaces/components/edit-workspace-form.tsx
+++ src/features/workspaces/components/edit-workspace-form.tsx
@@ -73,144 +73,170 @@ export const EditWorkSpaceForm = ({
   };
 
   return (
-    <Card className="w-full h-full border-none shadow-none">
-      <CardHeader className="flex flex-row justify-between items-center p-7  space-y-0">
-        <CardTitle className="text-xl font-bold">
-          {/* یک فضای کاری جدید ایجاد کنید */}
-          {initialValues.name}
-        </CardTitle>
-        <Button
-          size="sm"
-          variant="secondary"
-          onClick={
-            onCancel
-              ? onCancel
-              : () => router.push(`/workspaces/${initialValues.$id}`)
-          }
-        >
-          برگشت
-          <ArrowLeftIcon className="size-4 mr-2" />
-        </Button>
-      </CardHeader>
-      <div className="px-7">
-        <DottedSeparator />
-      </div>
-      <CardContent className="p-7">
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)}>
-            <div className="flex flex-col gap-y-4">
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>نام فضای کاری</FormLabel>
-                    <FormControl>
-                      <Input {...field} placeholder="نام فضای کاری" />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="image"
-                render={({ field }) => (
-                  <div className="felx flex-col gap-y-2">
-                    <div className="flex items-center gap-x-5">
-                      {field.value ? (
-                        <div className="size-[72px] relative rounded-md overflow-hidden">
-                          <Image
-                            alt="Logo"
-                            fill
-                            className="object-cover"
-                            src={
-                              field.value instanceof File
-                                ? URL.createObjectURL(field.value)
-                                : field.value
-                            }
-                          />
-                        </div>
-                      ) : (
-                        <Avatar className="size-[72px]">
-                          <AvatarFallback>
-                            <ImageIcon className="size-[36px] text-neutral-400" />
-                          </AvatarFallback>
-                        </Avatar>
-                      )}
-                      <div className="flex flex-col">
-                        <p className="text-sm">آیکون فضای کاری</p>
-                        <p className="text-sm text-muted-foreground">
-                          JPEG,JPG,SVG,PNG max 1mb
-                        </p>
-                        <input
-                          className="hidden"
-                          type="file"
-                          accept=".jpg, .png, jpeg,.svg"
-                          ref={inputRef}
-                          onChange={handleImageChange}
-                          disabled={isPending}
-                        />
+    <div className="flex flex-col gap-y-4">
+      <Card className="w-full h-full border-none shadow-none">
+        <CardHeader className="flex flex-row justify-between items-center p-7  space-y-0">
+          <CardTitle className="text-xl font-bold">
+            {/* یک فضای کاری جدید ایجاد کنید */}
+            {initialValues.name}
+          </CardTitle>
+          <Button
+            size="sm"
+            variant="secondary"
+            onClick={
+              onCancel
+                ? onCancel
+                : () => router.push(`/workspaces/${initialValues.$id}`)
+            }
+          >
+            برگشت
+            <ArrowLeftIcon className="size-4 mr-2" />
+          </Button>
+        </CardHeader>
+        <div className="px-7">
+          <DottedSeparator />
+        </div>
+        <CardContent className="p-7">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <div className="flex flex-col gap-y-4">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>نام فضای کاری</FormLabel>
+                      <FormControl>
+                        <Input {...field} placeholder="نام فضای کاری" />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="image"
+                  render={({ field }) => (
+                    <div className="felx flex-col gap-y-2">
+                      <div className="flex items-center gap-x-5">
                         {field.value ? (
-                          <Button
-                            type="button"
-                            disabled={isPending}
-                            variant="destructive"
-                            size="xs"
-                            className="w-fit mt-2 p-2"
-                            onClick={() => {
-                              field.onChange(null);
-                              if (inputRef.current) {
-                                inputRef.current.value = "";
+                          <div className="size-[72px] relative rounded-md overflow-hidden">
+                            <Image
+                              alt="Logo"
+                              fill
+                              className="object-cover"
+                              src={
+                                field.value instanceof File
+                                  ? URL.createObjectURL(field.value)
+                                  : field.value
                               }
-                            }}
-                          >
-                            حذف عکس
-                          </Button>
+                            />
+                          </div>
                         ) : (
-                          <Button
-                            type="button"
-                            disabled={isPending}
-                            variant="teritary"
-                            size="xs"
-                            className="w-fit mt-2 p-2"
-                            onClick={() => inputRef.current?.click()}
-                          >
-                            آپلود عکس
-                          </Button>
+                          <Avatar className="size-[72px]">
+                            <AvatarFallback>
+                              <ImageIcon className="size-[36px] text-neutral-400" />
+                            </AvatarFallback>
+                          </Avatar>
                         )}
+                        <div className="flex flex-col">
+                          <p className="text-sm">آیکون فضای کاری</p>
+                          <p className="text-sm text-muted-foreground">
+                            JPEG,JPG,SVG,PNG max 1mb
+                          </p>
+                          <input
+                            className="hidden"
+                            type="file"
+                            accept=".jpg, .png, jpeg,.svg"
+                            ref={inputRef}
+                            onChange={handleImageChange}
+                            disabled={isPending}
+                          />
+                          {field.value ? (
+                            <Button
+                              type="button"
+                              disabled={isPending}
+                              variant="destructive"
+                              size="xs"
+                              className="w-fit mt-2 p-2"
+                              onClick={() => {
+                                field.onChange(null);
+                                if (inputRef.current) {
+                                  inputRef.current.value = "";
+                                }
+                              }}
+                            >
+                              حذف عکس
+                            </Button>
+                          ) : (
+                            <Button
+                              type="button"
+                              disabled={isPending}
+                              variant="teritary"
+                              size="xs"
+                              className="w-fit mt-2 p-2"
+                              onClick={() => inputRef.current?.click()}
+                            >
+                              آپلود عکس
+                            </Button>
+                          )}
+                        </div>
                       </div>
                     </div>
-                  </div>
-                )}
-              />
-            </div>
-            <div className="py-7">
-              <DottedSeparator />
-            </div>
-            <div className="flex items-center justify-between">
+                  )}
+                />
+              </div>
+              <div className="py-7">
+                <DottedSeparator />
+              </div>
+              <div className="flex items-center justify-between">
+                <Button
+                  type="submit"
+                  size="lg"
+                  variant="primary"
+                  disabled={isPending}
+                >
+                  ذخیره تغییرات{" "}
+                </Button>
+                <Button
+                  type="button"
+                  size="lg"
+                  variant="secondary"
+                  onClick={onCancel}
+                  disabled={isPending}
+                  className={cn(!onCancel && "invisible")}
+                >
+                  لغو
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+
+      <Card className="w-full h-full border-none shadow-none">
+        <CardContent className="p-7">
+          <div className="flex flex-col">
+            <h3 className="font-bold items-end">danger zone</h3>
+            <p className="text-sm text-muted-foreground">
+              حذف یک فضای کاری برگشت ناپذیر است و تمام داده های مرتبط را حذف می
+              کند
+            </p>
+            <div className="">
               <Button
-                type="submit"
-                size="lg"
-                variant="primary"
-                disabled={isPending}
-              >
-                ذخیره تغییرات{" "}
-              </Button>
-              <Button
+                className="mt-6 w-fit ml-auto"
+                size="sm"
+                variant="destructive"
                 type="button"
-                size="lg"
-                variant="secondary"
-                onClick={onCancel}
                 disabled={isPending}
-                className={cn(!onCancel && "invisible")}
+                onClick={() => {}}
               >
-                لغو
+                حذف
               </Button>
             </div>
-          </form>
-        </Form>
-      </CardContent>
-    </Card>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
   );
 };

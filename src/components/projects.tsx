12cb6a0d61234b7700652f35db5a120a01076193
ShakilaMhd <shakila1996mhd@gmail.com>
2025-01-09 20:24:13 +0330

    ADD projects component api hook schema to create the project for specific workspace

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -7,11 +7,14 @@ import { RiAddCircleFill } from "react-icons/ri";
 import { useGetProjects } from "@/features/projects/api/use-get-projects";
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 import { cn } from "@/lib/utils";
+import { useCreateProjectModal } from "@/features/projects/hooks/use-create-project-modal";
+import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 
 export const Projects = () => {
   const projectId = null; //todo projectId
 
   const pathname = usePathname();
+  const { open } = useCreateProjectModal();
   const workspaceId = useWorkspaceId();
   const { data } = useGetProjects({ workspaceId });
 
@@ -20,7 +23,7 @@ export const Projects = () => {
       <div className="flex items-center justify-between">
         <p className="text-xs text-neutral-500">پروژه ها</p>
         <RiAddCircleFill
-          onClick={() => {}}
+          onClick={open}
           className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition"
         />
       </div>
@@ -32,11 +35,12 @@ export const Projects = () => {
           <Link href={href} key={project.$id}>
             <div
               className={cn(
-                "flex items-center gap-2.5 p-2.5 rounded-md hover:opacity-75 transition cursor-pointer text-neutral-500 ",
+                "flex items-center gap-2.5 p-1 rounded-md hover:opacity-75 transition cursor-pointer text-neutral-500 ",
                 isActive && "bg-white shadow-sm hover:opacity-100 text-primary"
               )}
             >
-              <span className="truncate">{project.name }</span>
+              <ProjectAvatar image={project.imageUrl} name={project.name} />
+              <span className="truncate text-sm">{project.name}</span>
             </div>
           </Link>
         );
--- src/features/projects/server/route.ts
+++ src/features/projects/server/route.ts
@@ -3,108 +3,97 @@ import { sessionMiddleware } from "@/lib/session-middlware";
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { z } from "zod";
-import { DATABASE_ID, IMAGES_BUCKET_ID, PROJECTS_ID } from "@/config"
+import { DATABASE_ID, IMAGES_BUCKET_ID, PROJECTS_ID } from "@/config";
 import { ID, Query } from "node-appwrite";
-import { createProjectSchema } from "../schema";
-
+import { createProjectSchema } from "../schemas";
 
 const app = new Hono()
 
-    .post(
-        "/",
-        sessionMiddleware,
-        zValidator("form", createProjectSchema),
-        async (c) => {
-            const databases = c.get("databases")
-            const storage = c.get("storage")
-            const user = c.get("user")
+  .post(
+    "/",
+    sessionMiddleware,
+    zValidator("form", createProjectSchema),
+    async (c) => {
+      const databases = c.get("databases");
+      const storage = c.get("storage");
+      const user = c.get("user");
 
-            const { name, image, workspaceId } = c.req.valid("form")
+      const { name, image, workspaceId } = c.req.valid("form");
 
-            const member = await getMember({
-                databases,
-                workspaceId,
-                userId: user.$id
-            })
-            if (!member) {
-                return c.json({ error: "Unauthorized" }, 401)
-            }
+      const member = await getMember({
+        databases,
+        workspaceId,
+        userId: user.$id,
+      });
+      if (!member) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
 
+      let uploadedImagesUrl: string | undefined;
 
-            let uploadedImagesUrl: string | undefined
+      if (image instanceof File) {
+        const file = await storage.createFile(
+          IMAGES_BUCKET_ID,
+          ID.unique(),
+          image
+        );
 
-            if (image instanceof File) {
-                const file = await storage.createFile(
-                    IMAGES_BUCKET_ID,
-                    ID.unique(),
-                    image
-                )
+        const arrayBuffer = await storage.getFilePreview(
+          IMAGES_BUCKET_ID,
+          file.$id
+        );
 
-                const arrayBuffer = await storage.getFilePreview(
-                    IMAGES_BUCKET_ID,
-                    file.$id
-                )
+        uploadedImagesUrl = `data:image/png;base64,${Buffer.from(
+          arrayBuffer
+        ).toString("base64")}`;
+      }
 
-
-                uploadedImagesUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`
-            }
-
-            const project = await databases.createDocument(
-                DATABASE_ID,
-                PROJECTS_ID,
-                ID.unique(),
-                {
-                    name: name,
-                    userId: user.$id,
-                    imageUrl: uploadedImagesUrl,
-                    workspaceId
-                }
-            )
-
-            return c.json({ data: project })
-
+      const project = await databases.createDocument(
+        DATABASE_ID,
+        PROJECTS_ID,
+        ID.unique(),
+        {
+          name: name,
+          imageUrl: uploadedImagesUrl,
+          workspaceId,
         }
-    )
+      );
 
+      return c.json({ data: project });
+    }
+  )
 
-    .get(
-        "/",
-        sessionMiddleware,
-        zValidator("query", z.object({ workspaceId: z.string() })),
-        async (c) => {
-            const user = c.get("user")
-            const databases = c.get("databases")
+  .get(
+    "/",
+    sessionMiddleware,
+    zValidator("query", z.object({ workspaceId: z.string() })),
+    async (c) => {
+      const user = c.get("user");
+      const databases = c.get("databases");
 
-            const { workspaceId } = c.req.valid("query")
+      const { workspaceId } = c.req.valid("query");
 
-            if (!workspaceId) {
-                return c.json({ error: "missing workspace id" }, 400)
-            }
+      if (!workspaceId) {
+        return c.json({ error: "missing workspace id" }, 400);
+      }
 
-            const member = await getMember({
-                databases,
-                workspaceId,
-                userId: user.$id
-            })
+      const member = await getMember({
+        databases,
+        workspaceId,
+        userId: user.$id,
+      });
 
-            if (!member) {
-                return c.json({ error: "Unauthorized" }, 401)
-            }
+      if (!member) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
 
-            const projects = await databases.listDocuments(
-                DATABASE_ID,
-                PROJECTS_ID,
-                [
-                    Query.equal("workspaceId", workspaceId),
-                    Query.orderDesc("$createdAt")
-                ]
-            )
+      const projects = await databases.listDocuments(DATABASE_ID, PROJECTS_ID, [
+        Query.equal("workspaceId", workspaceId),
+        Query.orderDesc("$createdAt"),
+      ]);
 
-            return c.json({ data: projects })
-        }
-    )
+      return c.json({ data: projects });
+    }
+  );
 
-
-
-
-export default app
+export default app;

a74a8ab94270ab9fadf61b63d0e3e6749d7f544f
ShakilaMhd <shakila1996mhd@gmail.com>
2025-01-14 01:05:02 +0330

    ADD task-actions in the diffrent page

---
+++ src/features/tasks/components/task-actions.tsx
@@ -0,0 +1,34 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ExternalLinkIcon } from "lucide-react";
+import React from "react";
+
+interface TaskActionsProps {
+  id: string;
+  projectId: string;
+  children: React.ReactNode;
+}
+
+export const TaskActions = ({ id, projectId, children }: TaskActionsProps) => {
+  return (
+    <div className="flex justify-end">
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-48">
+          <DropdownMenuItem
+            onClick={() => {}}
+            disabled={false}
+            className="font-medium p-[10px]"
+          >
+            <ExternalLinkIcon className="size-4 mr-2 stroke-2" />
+            جزئیات تسک
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};

ba4580f85bf45513eaa9e27e0c0a1f68fc1a6bd9
ShakilaMhd <shakila1996mhd@gmail.com>
2025-01-16 11:39:33 +0330

    ADD control the events with switching in the month

--- src/features/tasks/components/data-calendar.tsx
+++ src/features/tasks/components/data-calendar.tsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+
+import { CalendarIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+
 import {
   format,
   getDay,
@@ -16,6 +19,7 @@ import { Task } from "../types";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import "./data-calendar.css";
 import { EventCard } from "./event-card";
+import { Button } from "@/components/ui/button";
 
 const locales = {
   "fa-IR": faIR,
@@ -32,6 +36,38 @@ const localizer = dateFnsLocalizer({
 interface DataCalendarProps {
   data: Task[];
 }
+
+interface CustomToolbarProps {
+  date: Date;
+  onNavigate: (action: "PREV" | "NEXT" | "TODAY") => void;
+}
+
+export const CustomToolbar = ({ date, onNavigate }: CustomToolbarProps) => {
+  return (
+    <div className="flex mb-4 gap-x-2 items-center w-full lg:w-auto justify-center lg:justify-start">
+      <Button
+        onClick={() => onNavigate("PREV")}
+        variant="secondary"
+        size="icon"
+        // className="flex items-center"
+      >
+        <ChevronRightIcon className="size-4 " />
+      </Button>
+      <div className="flex items-center border border-input rounded-md px-3 py-2 h-8 justify-center w-full lg:w-auto">
+        <CalendarIcon className="size-4 mr-2" />
+        <p className="text-sm">{format(date, "MMM YYY")}</p>
+      </div>
+      <Button
+        onClick={() => onNavigate("NEXT")}
+        variant="secondary"
+        size="icon"
+        // className="flex items-center"
+      >
+        <ChevronLeftIcon className="size-4 " />
+      </Button>
+    </div>
+  );
+};
 
 export const DataCalendar = ({ data }: DataCalendarProps) => {
   const [value, setValue] = useState(
@@ -82,6 +118,9 @@ export const DataCalendar = ({ data }: DataCalendarProps) => {
             project={event.project}
             status={event.status}
           />
+        ),
+        toolbar: () => (
+          <CustomToolbar date={value} onNavigate={handleNavigate} />
         ),
       }}
     />
